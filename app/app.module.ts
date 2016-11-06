7bdbaa04520b6334b1b58601f9b38cc896f899da
Lasse Riess <lasse.riess@gmail.com>
2016-11-06 08:34:37 +0100

    merge correction

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,12 +4,23 @@ import { NativeScriptModule } from "nativescript-angular/platform";
 import { OffersPage } from "./pages/offers/offers";
 import { AppComponent } from "./app.component";
 
+import { BarcodeScanner } from 'nativescript-barcodescanner';
+
+import { ProfilePage } from "./pages/Profile/profile"
+import { SalePage } from "./pages/sale/sale"
+
+import { ProductModalView } from "./pages/sale/productModal"
+import { BrandModalView } from "./pages/sale/brandModal"
 @NgModule({
     imports: [NativeScriptModule],
     declarations: [
         AppComponent,
         OffersPage,
+        ProfilePage, 
+        SalePage,ProductModalView, BrandModalView
     ],
     bootstrap: [AppComponent],
+    providers: [BarcodeScanner],
+    entryComponents: [ProductModalView, BrandModalView]
 })
 export class AppModule { }
--- app/main.ts
+++ app/main.ts
@@ -1,25 +1,5 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import { platformNativeScriptDynamic, NativeScriptModule } from "nativescript-angular/platform";
-import { NgModule } from "@angular/core";
-import { AppComponent } from "./app.component";
-import { BarcodeScanner } from 'nativescript-barcodescanner';
-
-import { ProfilePage } from "./pages/Profile/profile"
-import { SalePage } from "./pages/sale/sale"
-
-import { ProductModalView } from "./pages/sale/productModal"
-import { BrandModalView } from "./pages/sale/brandModal"
-
-@NgModule({
-    declarations: [AppComponent, 
-    ProfilePage, 
-    SalePage,ProductModalView, BrandModalView],
-    bootstrap: [AppComponent],
-    imports: [NativeScriptModule],
-    providers: [BarcodeScanner],
-    entryComponents: [ProductModalView, BrandModalView]
-})
-
 class AppComponentModule {}
 
 import { AppModule } from "./app.module";
